689923df5d9b50acad3f5d85b272ff2913bc5f82
Bartosz Ociepka <bociepka1@gmail.com>
2020-05-14 18:07:59 +0200

    added GET student methods

--- rest-api/src/main/java/pl/edu/agh/soa/api/StudentRestService.java
+++ rest-api/src/main/java/pl/edu/agh/soa/api/StudentRestService.java
@@ -3,7 +3,6 @@ package pl.edu.agh.soa.api;
 import pl.edu.agh.soa.model.Student;
 import pl.edu.agh.soa.model.StudentsDAO;
 
-import javax.print.attribute.standard.Media;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -23,5 +22,30 @@ public class StudentRestService {
         List<Student> resultList = myDAO.getAllStudents();
 
         return Response.status(Response.Status.OK).entity(resultList).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    public Response getStudentById(@PathParam("id") int id) {
+        if (myDAO.getStudentById(id) == null){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        else {
+            return Response.status(Response.Status.OK).entity(myDAO.getStudentById(id)).build();
+        }
+    }
+
+    @POST
+    @Path("/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addStudent(Student student) {
+            try {
+                myDAO.addStudent(student);
+            }catch (Exception e){
+                return Response.status(Response.Status.CONFLICT).entity(myDAO.getStudentById(student.getId())).build();
+            }
+            return Response.status(Response.Status.OK).entity(student).build();
     }
 }
